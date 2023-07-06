eab82c0ab9665dae417c341854d4fa376e3039c5
Çınar <115741803+Lowrinn@users.noreply.github.com>
2023-07-06 13:55:26 +0300

    a

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+import Contact from './component/contact';
+function App() {
+  return (
+    <div class="App">
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
